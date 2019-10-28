d35109486531393301d5b9bf86d4ba5d5cfb7d65
Sovello Hildebrand <sovellohpmgani@gmail.com>
2019-10-27 23:15:36 -0400

    Initial commit

---
+++ exercise.py
@@ -0,0 +1,35 @@
+
+
+class Exercise:
+    """
+    An exercise:
+    - Part of the domain
+
+    - An exercise should keep its own state including passes, percentage score, etc
+    - This class shouldn't know anything about a widget  
+    """
+    def __init__(self, name, text):
+        self.name = name
+        self.text = text
+        self.passes = 0
+        self.current_char_index=0
+        self.length = len(self.text)
+    
+    def next_char(self):
+        self.current_char_index += 1
+
+    @property
+    def current_char(self):
+        return self.text[self.current_char_index]
+
+    def add_passes(self):
+        self.passes += 1
+
+    @property
+    def percentage_score(self):
+        return f"{self.passes / self.length * 100:.2f}"
+
+    @property
+    def live_score(self):
+        return f"{self.passes / (self.current_char_index) * 100:.2f}"
+    
